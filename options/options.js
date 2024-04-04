2b4661fb77d047375fe1ee4f304cd4b889a0c908
Crowdbotics <team@crowdbotics.com>
2024-04-04 22:33:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-truth-47391",
+  url: "https://autumn-truth-47391.botics.co",
+  api: "https://autumn-truth-47391.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
